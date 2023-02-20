7eba34f1c76d8b7e9bc0d2bf1ddaeccf63103059
Wesley de Sousa Sampaio <124082412+wesleysampaio13@users.noreply.github.com>
2023-02-20 09:43:48 -0300

    atualização

--- financeiro.c
+++ financeiro.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main(void){
+    char mes[15];
     int valor1 = 0;
     int valor2 = 0;
     int resultado = 0;
     
+    printf("\n Informe o mês: ");
+    fflush(stdin);
+    fgets(mes, 15, stdin);
+
     printf("\n Digite o primeiro valor: ");
     scanf("%d", &valor1);
     printf("\n Digite o segundo valor: ");
@@ -11,5 +16,5 @@ int main(void){
 
     resultado = valor1 % valor2;
 
-    printf("\n O lucro obtido durante o mês de janeiro = %d", resultado);
+    printf("\n O lucro obtido durante o mês de %s = %d", mes, resultado);
 }
