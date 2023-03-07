5dfbc2107a16c126c787ab8f131c7dc66ef94c91
Wesley de Sousa Sampaio <124082412+wesleysampaio13@users.noreply.github.com>
2023-03-06 21:07:29 -0300

    Atualization

--- par.c
+++ par.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 int main(void){
     int y;
-    for (y = 0; y <= 10; y++){
+    for (y = 0; y <= 10;y += 2){
         printf("\n y = %d", y);
-    }        
+    }
     return 0;
 }
